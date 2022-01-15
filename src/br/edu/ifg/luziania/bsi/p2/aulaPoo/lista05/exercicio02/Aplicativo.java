91e435fa188cc13fa564fe87f3f4e0af6090759b
weu-souza <weuller1561@gmail.com>
2022-01-14 21:27:27 -0300

    ...

--- src/br/edu/ifg/luziania/bsi/p2/aulaPoo/lista05/exercicio02/Aplicativo.java
+++ src/br/edu/ifg/luziania/bsi/p2/aulaPoo/lista05/exercicio02/Aplicativo.java
@@ -1,18 +1,13 @@
 package br.edu.ifg.luziania.bsi.p2.aulaPoo.lista05.exercicio02;
 
 
-
 public class Aplicativo {
     public static void main(String[] args) {
-
-
-
 
 
         Proprietario proprietario = new Proprietario();
 
         System.out.println(proprietario.dadosProprietarios());
-
 
 
     }
