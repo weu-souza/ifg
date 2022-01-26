3c05ec3d80630be42c6701a8735e0957debb9a06
weu-souza <weuller1561@gmail.com>
2022-01-25 21:52:15 -0300

    ...

--- src/br/edu/ifg/luziania/bsi/p2/aulaPoo/prova01/Aplicativo.java
+++ src/br/edu/ifg/luziania/bsi/p2/aulaPoo/prova01/Aplicativo.java
@@ -4,6 +4,6 @@ package br.edu.ifg.luziania.bsi.p2.aulaPoo.prova01;
 public class Aplicativo {
     public static void main(String[] args) {
         Lista l = new Lista();
-        System.out.println(l);
+        l.exibirLista();
     }
 }
--- src/br/edu/ifg/luziania/bsi/p2/aulaPoo/prova01/Lista.java
+++ src/br/edu/ifg/luziania/bsi/p2/aulaPoo/prova01/Lista.java
@@ -3,7 +3,7 @@ package br.edu.ifg.luziania.bsi.p2.aulaPoo.prova01;
 import java.util.Scanner;
 
 public class Lista {
-    public String exibirLista(){
+    public void exibirLista(){
         Scanner listar = new Scanner(System.in);
         int escolher;
         Carro carro = new Carro(4, 120, 13, 4, "preto", "FXZ-4765", 50, 2000);
@@ -13,47 +13,49 @@ public class Lista {
         Balao balao = new Balao(5, 5, 10, 5, 5);
         Barco barco = new Barco(40, 100, 20, "rio", 50, 3000);
         Navio navio = new Navio(30, 20, 4, "mar", 50, 30);
-        System.out.println("escolha um veiculo");
-        System.out.println("1-carro\n"+"2-moto\n"+"3-caminhão\n"+"4-barco\n"+"5-navio\n"+"6-aviao\n"+"7-balão");
-        escolher = listar.nextInt();
+        for (int i = 0; i < 7; i++) {
+            System.out.println("escolha um veiculo");
+            System.out.println("1-carro\n"+"2-moto\n"+"3-caminhão\n"+"4-barco\n"+"5-navio\n"+"6-aviao\n"+"7-balão");
+            escolher = listar.nextInt();
 
-        if(escolher == 1){
-            carro.exibirObjeto();
-            carro.capacidadeDePessoasDoTransporte();
-            carro.exibirVelocidadeMaxima();
-            carro.exibirCarro();
+            if(escolher == 1){
+                carro.exibirObjeto();
+                carro.capacidadeDePessoasDoTransporte();
+                carro.exibirVelocidadeMaxima();
+                carro.exibirCarro();
+            }
+            else if(escolher == 2){
+                motocicleta.exibirObjeto();
+                motocicleta.exibirVelocidadeMaxima();
+                motocicleta.capacidadeDePessoasDoTransporte();
+                motocicleta.dadosMoto();
+            }else if(escolher == 3){
+                caminhao.exibirObjeto();
+                caminhao.exibirVelocidadeMaxima();
+                caminhao.capacidadeDePessoasDoTransporte();
+                caminhao.exibirCaminhão();
+            }else if(escolher == 4){
+                barco.objeto();
+                barco.exibirVelocidadeMaxima();
+                barco.capacidadeDePessoasDoTransporte();
+                barco.dadosBarco();
+            }else if(escolher == 5){
+                navio.objeto();
+                navio.exibirVelocidadeMaxima();
+                navio.capacidadeDePessoasDoTransporte();
+                navio.dadosNavio();
+            }else if(escolher == 6){
+                aviao.objeto();
+                aviao.exibirVelocidadeMaxima();
+                aviao.capacidadeDePessoasDoTransporte();
+                aviao.exibirDadosAviao();
+            }else if(escolher == 7){
+                balao.objeto();
+                balao.capacidadeDePessoasDoTransporte();
+                balao.exibirVelocidadeMaxima();
+                balao.exibirDadosBalao();
+            }
         }
-        else if(escolher == 2){
-            motocicleta.exibirObjeto();
-            motocicleta.exibirVelocidadeMaxima();
-            motocicleta.capacidadeDePessoasDoTransporte();
-            motocicleta.dadosMoto();
-        }else if(escolher == 3){
-            caminhao.exibirObjeto();
-            caminhao.exibirVelocidadeMaxima();
-            caminhao.capacidadeDePessoasDoTransporte();
-            caminhao.exibirCaminhão();
-        }else if(escolher == 4){
-            barco.objeto();
-            barco.exibirVelocidadeMaxima();
-            barco.capacidadeDePessoasDoTransporte();
-            barco.dadosBarco();
-        }else if(escolher == 5){
-            navio.objeto();
-            navio.exibirVelocidadeMaxima();
-            navio.capacidadeDePessoasDoTransporte();
-            navio.dadosNavio();
-        }else if(escolher == 6){
-            aviao.objeto();
-            aviao.exibirVelocidadeMaxima();
-            aviao.capacidadeDePessoasDoTransporte();
-            aviao.exibirDadosAviao();
-        }else if(escolher == 7){
-            balao.objeto();
-            balao.capacidadeDePessoasDoTransporte();
-            balao.exibirVelocidadeMaxima();
-            balao.exibirDadosBalao();
-        }
 
 
 
@@ -63,11 +65,9 @@ public class Lista {
 
 
 
-        return "";
+
+
     }
 
-    @Override
-    public String toString() {
-        return exibirLista();
-    }
+
 }
